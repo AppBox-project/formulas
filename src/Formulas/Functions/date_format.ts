462afa0b4a468f636f477b8664820b9719aea7de
Vic van Cooten <duveaux1@gmail.com>
2020-11-19 15:27:01 +0100

    Type fixes

--- src/Formulas/Functions/date_format.ts
+++ src/Formulas/Functions/date_format.ts
@@ -10,19 +10,18 @@ import Formula from "../../Formula";
  * Calculates the difference between left and right according to date-fns.
  */
 export default {
-  execute: (fArgs, data, formula: Formula) =>
+  execute: (args, data, formula: Formula) =>
     new Promise((resolve) => {
-      let date = fArgs[0].str
-        ? typeof fArgs[0].str == "string"
-          ? typeof data[fArgs[0].str] === "string"
-            ? parseISO(data[fArgs[0].str])
-            : data[fArgs[0].str]
-          : fArgs[0]
-        : data[fArgs[0]];
+      let date = args[0].str
+        ? typeof args[0].str == "string"
+          ? typeof data[args[0].str] === "string"
+            ? parseISO(data[args[0].str])
+            : data[args[0].str]
+          : args[0]
+        : data[args[0]] || args[0];
 
       if (typeof date === "string") date = parseISO(date);
-
-      resolve(format(date, fArgs[1].str));
+      resolve(format(date, args[1].str));
     }),
   onCompile: (fArguments) => {
     // Mark argument 0 as required
